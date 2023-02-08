716fc23026c7894319bc452adc510b4ddd07c659
cc-aeya <aaminah.x@icloud.com>
2023-02-08 15:48:07 +0000

    onClick function created above and stored in variable

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ function App() {
 
   const title = 'My App Logo'
   const [showMain, setShowMain] = useState(true);
+  
+  // creating this anonymous func is best practise for event listeners
+  const toggleMain = () => setShowMain(!showMain);
+
+  // if statement can be included instead of the short hand
+    // if (showMain) {
+      // setShowMain(false)
+      // else setShowMain(true)
+    
+  
 
   return (
     < >
       <Header title={title} />
-      <button onClick={() => {
-        setShowMain(true)
-      }}
-      >Main</button>
+      <button onClick={toggleMain}>Main</button>
      
      {showMain && <Main string={"My in-line Heading"}/>}
       <Footer />
