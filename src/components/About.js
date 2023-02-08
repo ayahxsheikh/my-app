1a73273f8f6c0ee4bb8a4fe2adb7a196c350e66d
cc-aeya <aaminah.x@icloud.com>
2023-02-08 14:10:09 +0000

    component activity

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+
+function About (){
+    return(
+        <>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum odio excepturi id harum est non mollitia iure debitis nesciunt rerum? Nesciunt pariatur sequi fuga voluptatibus aspernatur, numquam blanditiis dignissimos ab.</p>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum odio excepturi id harum est non mollitia iure debitis nesciunt rerum? Nesciunt pariatur sequi fuga voluptatibus aspernatur, numquam blanditiis dignissimos ab.</p>
+        </>
+    )
+}
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+
+function Footer(){
+    return(
+        <footer>
+            <button>FB</button>
+            <button>IG</button>
+            <button>TW</button>
+        </footer>
+    )
+}
+
+export default Footer;
